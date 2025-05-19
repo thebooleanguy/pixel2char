c9d1024ade70a79ce7316a938512b67a11215947
thebooleanguy <tharindus0601@gmail.com>
2025-05-19 10:17:17 +0530

    Downscale frame 640x480p to 64x48p

--- pixel2char.py
+++ pixel2char.py
@@ -5,8 +5,11 @@ cam = cv2.VideoCapture(0)
 
 ret, frame = cam.read()
 print(frame.shape)
+
+downscaled_frame = cv2.resize(frame, (64, 48), interpolation = cv2.INTER_AREA)
+
 # Output frame in a GUI
-cv2.imshow("CamTest", frame)
+cv2.imshow("DownscaleTest", downscaled_frame)
 # Hold output for a set number of milliseconds
 cv2.waitKey(5000)
 
