06c15eefe0214ac9a079cfd033edb2d3371aa130
thebooleanguy <tharindus0601@gmail.com>
2025-05-19 10:21:08 +0530

    Convert downscaled image into grayscale

--- pixel2char.py
+++ pixel2char.py
@@ -7,9 +7,10 @@ ret, frame = cam.read()
 print(frame.shape)
 
 downscaled_frame = cv2.resize(frame, (64, 48), interpolation = cv2.INTER_AREA)
+grayscaled_frame = cv2.cvtColor(downscaled_frame, cv2.COLOR_BGR2GRAY)
 
 # Output frame in a GUI
-cv2.imshow("DownscaleTest", downscaled_frame)
+cv2.imshow("DownscaleTest", grayscaled_frame)
 # Hold output for a set number of milliseconds
 cv2.waitKey(5000)
 
